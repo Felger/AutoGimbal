e6ad3fe5e3920f89e18d42dd15fb37c3356c7865
Felger <jasrapp@gmail.com>
2014-07-06 10:46:40 -0500

    Interface to IR working
    Got the interface to infernal robotics working, built the function to
    command the robotics to a set point.

--- AutoGimbal.cs
+++ AutoGimbal.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using KSP;
 using InfernalRobotics;
 using MuMech;
+
 
 namespace AutoGimbal
 {
@@ -22,112 +22,214 @@ namespace AutoGimbal
         public string Target = "none";
         [KSPField(isPersistant = true, guiActive = true)]
         public float rotation = 0;
+        [KSPField(isPersistant = true, guiActive = true)]
+        public int TrackingState = 0;
+        [KSPField(isPersistant = true, guiActive = true)]
+        public int MoveState = 0;
 
+        private float min_Delta;
 
+        public AutoGimbalManager manager;
 
+        Vessel currentVess;
         List<CelestialBody> body = FlightGlobals.Bodies;
         List<Vessel> ship = FlightGlobals.Vessels;
-
-
 
         public override void OnStart(StartState start)
         {
             print("Celestial Body count:" + body.Count);
             print("Vessel count:" + ship.Count);
+            currentVess = FlightGlobals.fetch.activeVessel;
         }
-        //This override to the OnUpdate function runs on every physics frame, and updates all values and calculations we're interested in.  
+
+        //This override to the OnUpdate function runs on every physics frame, and updates all values and calculations we're interested in.
         public override void OnUpdate()
         {
-            Vessel currentVess = FlightGlobals.fetch.activeVessel;
-
             var Robotics = part.FindModulesImplementing<MuMechToggle>().First();
-
-
-       
-            //rotation = paMuMech.MuMechToggle.rotation;
+            rotation = Robotics.rotation;
+            min_Delta = float.Parse(Robotics.stepIncrement) * 2;
 
             Vector3d position = currentVess.GetWorldPos3D();
             Vector3d t_pos = body[2].position;
             Quaternion q = currentVess.ReferenceTransform.rotation;
             Vector3d delta = (position - t_pos);
-
+            
             Target = body[2].GetName();
+
+            TrackingState = manager.TrackingState;
+            float Rotation_Target = manager.rotationTarget;
+            MoveState = IR_Commander(TrackingState, MoveState, Rotation_Target);
 
             pos_X = delta.x;
             pos_Y = delta.y;
             pos_Z = delta.z;
         }
 
+        //IR_Commander interfaces with Infernal Robotics to command motion to a specific point as specified on input.
+        //      TrackingState is input from the GUI, commanded by user.
+        //      MoveState keeps track of whether the part is in motion, and what direction.
+        //      Rotation_Target is specified by math (or user input)
+        public int IR_Commander(int TrackingState, int MoveState, float Rotation_Target)
+        {
+            var Robotics = part.FindModulesImplementing<MuMechToggle>().First();
+            float Delta = Math.Abs(Robotics.rotation - Rotation_Target);
+
+            if (TrackingState == 1) //User has enabled motion, to track to rotation target.
+            {
+                //Logic to catch either case with the servo inverted or non-inverted.
+                if (((Robotics.rotation > Rotation_Target && !Robotics.invertAxis) ||
+                     (Robotics.rotation < Rotation_Target && Robotics.invertAxis))
+                    && Delta > min_Delta && MoveState == 0) //Movestate = 0 defines stopped
+                {
+                    Robotics.moveFlags |= 0x100; //Start negative motion
+                    MoveState = 1;  //MoveState = 1 defines negative motion
+                }
+                else if (((Robotics.rotation < Rotation_Target && !Robotics.invertAxis) ||
+                     (Robotics.rotation > Rotation_Target && Robotics.invertAxis))
+                    && Delta > min_Delta && MoveState == 0) //Movestate = 0 defines stopped
+                {
+                    Robotics.moveFlags |= 0x200; //Start positive motion
+                    MoveState = 2;  //MoveState = 2 defines positive motion
+                }
+                else if (((Robotics.rotation <= Rotation_Target && !Robotics.invertAxis) ||
+                    (Robotics.rotation >= Rotation_Target && Robotics.invertAxis))
+                    && MoveState == 1)
+                {
+                    Robotics.moveFlags &= ~0x100; //Stop negative motion
+                    MoveState = 0; 
+                }
+                else if (((Robotics.rotation >= Rotation_Target && !Robotics.invertAxis) ||
+                    (Robotics.rotation <= Rotation_Target && Robotics.invertAxis))
+                    && MoveState == 2)
+                {
+                    Robotics.moveFlags &= ~0x200; //Stop positive motion
+                    MoveState = 0; //MoveState = 0 defines negative motion
+                }
+            }
+            else if (TrackingState == 0)
+            {
+                if (MoveState == 2)
+                {
+                    Robotics.moveFlags &= ~0x200; //Stop positive motion
+                    MoveState = 0; //MoveState = 0 defines negative motion
+                }
+                else if (MoveState == 1)
+                {
+                    Robotics.moveFlags &= ~0x100; //Stop negative motion
+                    MoveState = 0; 
+                }
+            }
+            return MoveState;
+        }//end IR_Commander
+
     }
 
-    public class AutoGimbal2 : PartModule
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    public class AutoGimbalManager : MonoBehaviour
     {
-        bool target_window_exist = false;
-
-        // This event will pop up the target selection window, once a target is selected tracking will beging for this gimbal.  
-        [KSPEvent(guiActive = true, guiName = "Select Target")]
-        public void ActivateEvent()
+        void Start()
         {
-            ScreenMessages.PostScreenMessage("Clicked Activate", 5.0f, ScreenMessageStyle.UPPER_CENTER);
-            target_window_exist = true;
-
-            RenderingManager.AddToPostDrawQueue(3, new Callback(drawGUI));//start the GUI
-
-
-            // This will hide the Activate event, and show the Deactivate event.
-            Events["ActivateEvent"].active = false;
-            Events["DeactivateEvent"].active = true;
+            FlightGlobals.Vessels.ForEach(vessel =>
+            {
+                vessel.FindPartModulesImplementing<AutoGimbal>().ForEach(gimbal =>
+                   { gimbal.manager = this; });
+            });
+            Debug.Log("[AutoGimbalManager] started");
         }
 
-        // By deselecting a target, you effectively stop tracking.
-        [KSPEvent(guiActive = true, guiName = "Deselect Target", active = false)]
-        public void DeactivateEvent()
-        {
-            ScreenMessages.PostScreenMessage("Clicked Deactivate", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+        //Make a GUI variable to define window position.
+        protected static Rect windowPosn;
+        private string rotationTargetString = "0.0";
+        public float rotationTarget;
+        private bool shipsToggle;
+        private bool bodiesToggle;
+        private bool guiEnabled = false;
+        //TrackingState stores the current state of the gimbal commanding.
+        //      0 = not tracking
+        //      1 = tracking
+        public int TrackingState;
+        IButton AutoGimbalButton;
 
-            // This will hide the Deactivate event, and show the Activate event.
-            if (target_window_exist == true)
-            {
-                RenderingManager.RemoveFromPostDrawQueue(3, new Callback(drawGUI)); //close the GUI
+        void Awake()
+        {
+            Debug.Log("[AutoGimbalManager] awake");
+            guiEnabled = false;
+            var scene = HighLogic.LoadedScene;
+            if (scene == GameScenes.FLIGHT)
+            {//Need to check at some point if we need to reload groups from vessel
             }
-
-            Events["ActivateEvent"].active = true;
-            Events["DeactivateEvent"].active = false;
+            if (ToolbarManager.ToolbarAvailable)
+            {
+                AutoGimbalButton = ToolbarManager.Instance.add("Felger", "AutoGimbalButton");
+                AutoGimbalButton.TexturePath = "AutoGimbal/Textures/icon_button";
+                AutoGimbalButton.ToolTip = "AutoGimbal Manager";
+                AutoGimbalButton.Visibility = new GameScenesVisibility(GameScenes.EDITOR, GameScenes.SPH, GameScenes.FLIGHT);
+                AutoGimbalButton.OnClick += (e) => guiEnabled = !guiEnabled;
+                AutoGimbalButton.Visible = true;
+            }
+            else
+            {
+                guiEnabled = true;
+            }
         }
 
 
-        protected Rect windowPos;
-
-        private void WindowGUI(int windowID)
+        //Create the OnDraw function called above into the rendering queue.  This will create the GUI and collect actions input to the GUI.
+        void OnGUI()
         {
-            GUIStyle mySty = new GUIStyle(GUI.skin.button);
-            mySty.normal.textColor = mySty.focused.textColor = Color.white;
-            mySty.hover.textColor = mySty.active.textColor = Color.yellow;
-            mySty.onNormal.textColor = mySty.onFocused.textColor = mySty.onHover.textColor = mySty.onActive.textColor = Color.green;
-            mySty.padding = new RectOffset(8, 8, 8, 8);
-
-            GUILayout.BeginVertical();
-            if (GUILayout.Button("DESTROY", mySty, GUILayout.ExpandWidth(true)))//GUILayout.Button is "true" when clicked
+            //Define the default window position, if no changes have been made:
+            if(windowPosn.x == 0 && windowPosn.y == 0)
             {
-                ScreenMessages.PostScreenMessage("DESTROYED", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+                windowPosn = new Rect(Screen.width - 300, 50, 10, 10);
+            }
+            var scene = HighLogic.LoadedScene;
+            if (scene == GameScenes.FLIGHT)
+            {
+                //Creates a new box, specifying the position to follow the defined position above.
+                if (guiEnabled)
+                {
+                    windowPosn = GUILayout.Window(0, windowPosn, WindowMaker, "AutoGimbal");
+                }
+            }
+        }
+        protected void WindowMaker(int windowID)
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label("Target Angle:");
+
+            rotationTargetString = GUILayout.TextField(rotationTargetString);
+            GUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Start"))
+            {
+                rotationTarget = float.Parse(rotationTargetString);
+                TrackingState = 1;
+            }
+            if(GUILayout.Button("Stop"))
+            {
+                TrackingState = 0;
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            shipsToggle = GUILayout.Toggle(shipsToggle,"Ships","Button");
+            bodiesToggle = GUILayout.Toggle(bodiesToggle, "Bodies","Button");
+            GUILayout.EndHorizontal();
+            if (ToolbarManager.ToolbarAvailable)
+            {
+                if (GUILayout.Button("Close"))
+                {
+                    guiEnabled = false;
+                }
             }
             GUILayout.EndVertical();
-
-            //DragWindow makes the window draggable. The Rect specifies which part of the window it can by dragged by, and is 
-            //clipped to the actual boundary of the window. You can also pass no argument at all and then the window can by
-            //dragged by any part of it. Make sure the DragWindow command is AFTER all your other GUI input stuff, or else
-            //it may "cover up" your controls and make them stop responding to the mouse.
-            GUI.DragWindow(new Rect(0, 0, 10000, 20));
-
+            GUI.DragWindow();   //Make the GUI draggable
         }
-        private void drawGUI()
+    void OnDestroy()
+    {
+        if (ToolbarManager.ToolbarAvailable)
         {
-            GUI.skin = HighLogic.Skin;
-            windowPos = GUILayout.Window(1, windowPos, WindowGUI, "Self Destruct", GUILayout.MinWidth(100));
-            if ((windowPos.x == 0) && (windowPos.y == 0))//windowPos is used to position the GUI window, lets set it in the center of the screen
-            {
-                windowPos = new Rect(Screen.width / 2, Screen.height / 2, 10, 10);
-            }
+            AutoGimbalButton.Destroy();
         }
+    }
     }
 }
